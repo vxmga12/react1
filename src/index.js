710b68ac420d94506e98f1dda3ffb0331b89cbf5
Vasco Augusto <vxmga12@gmail.com>
2023-05-01 19:47:58 +0100

    Used axios to fetch data (json)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios'
 
 import App from './App';
+
+axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+        const notes = response.data
+        console.log(notes)
+    })
 
 const notes = [
     {
